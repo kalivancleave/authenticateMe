5007449e4081cdc9290a70fe982ed5786aaa540d
kalivancleave <kalivancleave@gmail.com>
2024-05-29 20:03:44 -0700

    add user input validation on user signup backend endpoint

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -5,10 +5,34 @@ const {setTokenCookie, requireAuth} = require('../../utils/auth');
 const {User} = require('../../db/models');
 const user = require('../../db/models/user');
 
+const {check} = require('express-validator');
+const {handleValidationErrors} = require('../../utils/validation');
+
 const router = express.Router();
 
+//validate sign up middleware
+const validateSignup = [
+  check('email')
+    .exists({checkFalsy: true})
+    .isEmail()
+    .withMessage('Please provide a valid email.'),
+  check('username')
+    .exists({checkFalsy: true})
+    .isLength({min: 4})
+    .withMessage('Please provide a username with at least 4 characters.'),
+  check('username')
+    .not()
+    .isEmail()
+    .withMessage('Username cannot be an email.'),
+  check('password')
+    .exists({checkFalsy: true})
+    .isLength({min: 6})
+    .withMessage('Password must be 6 characters or more.'),
+  handleValidationErrors
+];
+
 //User Signup API Route
-router.post('/', async (req, res, next) => {
+router.post('/', validateSignup, async (req, res, next) => {
   try {
     //deconstruct req.body
     const {email, password, username} = req.body;
